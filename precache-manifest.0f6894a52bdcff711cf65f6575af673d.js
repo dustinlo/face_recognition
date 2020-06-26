c8b881dcdfc444fd6b1a6bc9c1d8d37d0dced1ba
dustinlo <lo.ch@husky.neu.edu>
2020-06-26 01:55:10 -0700

    Updates

---
+++ precache-manifest.0f6894a52bdcff711cf65f6575af673d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4233690576f934602f20178aee28e0b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0035f9c67fa464598fc",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6fb556a3dff06e41d5a1",
+    "url": "/static/css/main.e4a460d0.chunk.css"
+  },
+  {
+    "revision": "a0035f9c67fa464598fc",
+    "url": "/static/js/2.96dbc243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96dbc243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb556a3dff06e41d5a1",
+    "url": "/static/js/main.84b6ff9b.chunk.js"
+  },
+  {
+    "revision": "9a8ab5b81512059c3879",
+    "url": "/static/js/runtime-main.fca6f91e.js"
+  }
+]);
