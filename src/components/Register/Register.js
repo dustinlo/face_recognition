04f8d115e9d560ab8c94387311caa30df3142f81
dustinlo <lo.ch@husky.neu.edu>
2020-06-26 02:28:19 -0700

    modified port

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -22,7 +22,7 @@ class Register extends Component  {
     }
 
     onSubmitSignIn = ()=> {
-        fetch('http://localhost:3000/register', {
+        fetch('https://stormy-bastion-45756.herokuapp.com/register', {
             method: 'post', 
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
